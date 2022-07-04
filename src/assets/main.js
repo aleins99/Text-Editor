f703512c340d30c2f3007c2d44a887bf3306a0f7
Alejandro Marin <alexword1893@gmail.com>
2022-07-04 16:59:47 -0400

    first commit

---
+++ src/assets/main.js
@@ -0,0 +1,33 @@
+const options = document.querySelectorAll("#navbar li");
+const inputss = document.getElementById("input-text");
+const resultArea = document.getElementById("result-text");
+const text = document.getElementById("text");
+var textClass = "";
+const alignButtons = ["left", "right", "center"];
+const alignItems = document.getElementsByClassName("align");
+for (let option of options) {
+  option.addEventListener("click", () => {
+    textClass = option.textContent.toLowerCase();
+    // remove all the class wich contains items of alignButtons
+    console.log(text);
+    for (let items of alignItems) {
+      if (
+        text.classList.contains(items.textContent.toLowerCase()) &&
+        alignButtons.includes(textClass) &&
+        items.textContent.toLowerCase() != textClass
+      ) {
+        items.classList.remove("active");
+        text.classList.remove(items.textContent.toLowerCase());
+      }
+    }
+    text.classList.toggle(textClass);
+    option.classList.toggle("active");
+  });
+}
+inputss.addEventListener("input", (event) => {
+  text.innerText = event.target.value;
+
+  console.log(textClass);
+});
+
+console.log(text);
